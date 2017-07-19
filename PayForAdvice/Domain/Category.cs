e154be2dbc0585cf88a641f3d115a52e6712ad62
WIRTEK\alexandra.tigau <alexandra_almasan27@yahoo.com>
2017-07-19 09:58:12 +0300

    Domain Classes

--- PayForAdvice/Domain/Category.cs
+++ PayForAdvice/Domain/Category.cs
@@ -1,6 +1,24 @@
-﻿namespace Domain
+﻿using System.Collections.Generic;
+
+namespace Domain
 {
     public class Category
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+
+        //foreign key
+        public virtual ICollection<User> Users { get; set; }
+
+        //constructor
+        public Category()
+        {
+            Users = new HashSet<User>();
+        }
+
+
+
     }
 }
