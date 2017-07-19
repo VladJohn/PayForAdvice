4c00d62f601654c4386df26841ce72d113777b50
WIRTEK\vlad.teodorescu <vladjohn2013@gmail.com>
2017-07-19 14:00:37 +0300

    Mappers and Models

---
+++ PayForAdvice/WebAPI/Mappings/RoleMapper.cs
@@ -0,0 +1,22 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAPI.Models;
+
+namespace WebAPI.Mappings
+{
+    public class RoleMapper
+    {
+        public static RoleModel MapRole(Role role)
+        {
+            return new RoleModel { Id = role.Id, Name = role.Name };
+        }
+
+        public static Role MapRoleDataModel (RoleModel role)
+        {
+            return new Role { Id = role.Id, Name = role.Name };
+        }
+    }
+}
--- PayForAdvice/WebAPI/Models/UploadModel.cs
+++ PayForAdvice/WebAPI/Models/UploadModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,9 @@ namespace WebAPI.Models
     public class UploadModel
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string UploadURL { get; set; }
 
         public int AnswerId { get; set; }
