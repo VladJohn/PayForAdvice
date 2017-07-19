f1506fdd0f253cc28989e8bb4ab5b6b01b723ffb
WIRTEK\vlad.teodorescu <vladjohn2013@gmail.com>
2017-07-19 10:13:19 +0300

    Idable

--- PayForAdvice/Domain/Upload.cs
+++ PayForAdvice/Domain/Upload.cs
@@ -6,9 +6,8 @@ using System.Threading.Tasks;
 
 namespace Domain
 {
-    public class Upload
+    public class Upload : Idable
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public string UploadURL { get; set; }
 
--- PayForAdvice/Domain/User.cs
+++ PayForAdvice/Domain/User.cs
@@ -6,9 +6,8 @@ using System.Threading.Tasks;
 
 namespace Domain
 {
-    public class User
+    public class User : Idable
     {
-        public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public string Bio { get; set; }
