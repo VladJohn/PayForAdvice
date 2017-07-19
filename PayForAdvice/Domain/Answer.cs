8b708ee2b36b55fa9fb7dd1ca5a58aed8ff874ca
WIRTEK\vlad.teodorescu <vladjohn2013@gmail.com>
2017-07-19 09:57:53 +0300

    Question, Answer, Comment and Upload classess

--- PayForAdvice/Domain/Answer.cs
+++ PayForAdvice/Domain/Answer.cs
@@ -1,6 +1,27 @@
-﻿namespace Domain
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain
 {
     public class Answer
     {
+        public int Id { get; set; }
+        public string AnswerText { get; set; }
+        public int Rating { get; set; }
+        public DateTime Date { get; set; }
+        public string Status { get; set; }
+        public string ReportText { get; set; }
+
+        public int QuestionId { get; set; }
+        public Question Question { get; set; }
+        public int UserIdResponder { get; set; }
+        public User UserResponder { get; set; }
+
+        public ICollection<Upload> Uploads { get; set; }
+
+        public Answer()
+        {
+            Uploads = new HashSet<Upload>();
+        }
     }
 }
