7384d9a4f91648d3c5eed8f7ff9264d4b686f830
WIRTEK\luiza.tanasie <luiza.tanasie197@gmail.com>
2017-07-19 12:48:50 +0300

    model

---
+++ PayForAdvice/WebAPI/Models/QuestionModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class QuestionModel
+    {
+        public int Id { get; set; }
+        public string QuestionText { get; set; }
+        public DateTime Date { get; set; }
+        public string Status { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
