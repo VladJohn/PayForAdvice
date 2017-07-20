2fa1ccbb724ff0e7b9247583347fe8269f439bee
WIRTEK\luiza.tanasie <luiza.tanasie197@gmail.com>
2017-07-20 16:53:30 +0300

    user controller

--- PayForAdvice/WebAPI/Controllers/UserController.cs
+++ PayForAdvice/WebAPI/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using WebAPI.Models;
 using WebAPI.Services;
 
 namespace WebAPI.Controllers
@@ -20,6 +21,63 @@ namespace WebAPI.Controllers
                 return NotFound();
             }
             return Ok(users);
+        }
+
+        //fix it
+        public IHttpActionResult GetLogIn(string username, string password)
+        {
+            var service = new UserService();
+            var found = service.LogIn(username, password);
+            if (found == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(found);
+        }
+
+        public IHttpActionResult PostUser([FromBody]UserModelForSignUp user)
+        {
+            var service = new UserService();
+            var userAdded = service.AddUser(user);
+            if (userAdded == null)
+            {
+                return NotFound();
+            }
+            return Ok(userAdded);
+        }
+
+        public IHttpActionResult PutUserDeleteStatus(int idUser)
+        {
+            var service = new UserService();
+            var user = service.DeleteUser(idUser);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+
+        public IHttpActionResult PutUserProfile([FromBody] UserModelForProfile user)
+        {
+            var service = new UserService();
+            var updated = service.UpdateUserProfile(user);
+            if (updated == null)
+            {
+                return NotFound();
+            }
+            return Ok(updated);
+        }
+
+        public IHttpActionResult GetUser(int id)
+        {
+            var service = new UserService();
+            var user = service.GetUser(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
         }
     }
 }
