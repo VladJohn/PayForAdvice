067f11276733f8915f0e0444aedbf309b7f3b43d
WIRTEK\alexandra.tigau <alexandra_almasan27@yahoo.com>
2017-07-20 13:51:54 +0300

    services2

---
+++ PayForAdvice/WebAPI/Services/RoleService.cs
@@ -0,0 +1,37 @@
+﻿using Domain;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAPI.Mappings;
+using WebAPI.Models;
+
+namespace WebAPI.Service
+{
+    public class RoleService
+    {
+        public RoleModel Add(RoleModel newRole)
+        {
+            using (var uw = new UnitOfWork())
+            {
+                var repo = uw.GetRepository<Role>();
+                repo.Add(RoleMapper.MapRoleDataModel(newRole));
+                uw.Save();
+            }
+            return newRole;
+        }
+
+        public RoleModel Get(int idUser)
+        {
+            using (var uw = new UnitOfWork())
+            {
+                var repo = uw.GetRepository<Role>();
+                var found = repo.GetAll().ToList().Where(x => x.Id.Equals(idUser)).FirstOrDefault();
+                if (found == null)
+                    return null;
+                return RoleMapper.MapRole(found);
+            }
+        }
+    }
+}
