81ba76c0d0489f5f06ecb95972d43e41c36924e7
WIRTEK\alexandra.tigau <alexandra_almasan27@yahoo.com>
2017-07-20 17:04:14 +0300

    contr

---
+++ PayForAdvice/WebAPI/Controllers/RoleController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using WebAPI.Service;
+
+namespace WebAPI.Controllers
+{
+    public class RoleController : ApiController
+    {
+        public IHttpActionResult Get(int IdRole)
+        {
+            var service = new RoleService();
+            var roles = service.Get(IdRole);
+            if (roles == null)
+                return NotFound();
+            return Ok(roles);
+        }
+    }
+}
