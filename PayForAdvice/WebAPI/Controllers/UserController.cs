0a9fc481b92dc376aa36a25d01e6dded51385320
WIRTEK\luiza.tanasie <luiza.tanasie197@gmail.com>
2017-07-20 12:32:38 +0300

    services

---
+++ PayForAdvice/WebAPI/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using WebAPI.Services;
+
+namespace WebAPI.Controllers
+{
+    public class UserController : ApiController
+    {
+        public IHttpActionResult GetUsersByCategory(int idCategory)
+        {
+            var service = new UserService();
+            var users = service.GetAdvicersByCategory(idCategory);
+            if (users == null)
+            {
+                return NotFound();
+            }
+            return Ok(users);
+        }
+    }
+}
---
+++ PayForAdvice/WebAPI/Services/UploadService.cs
@@ -0,0 +1,27 @@
+﻿using Domain;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAPI.Mappings;
+using WebAPI.Models;
+
+namespace WebAPI.Services
+{
+    public class UploadService
+    {
+        public UploadModel Register(UploadModel upload)
+        {
+            using (var uw = new UnitOfWork())
+            {
+                var repo = uw.GetRepository<Upload>();
+                repo.Add(UploadMapper.MapUploadDataModel(upload));
+                uw.Save();
+                return upload;
+            }
+        }
+
+
+    }
+}
