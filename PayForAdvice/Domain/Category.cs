997ff5580dfe55d0047b592d2fcc4d8361576e8b
WIRTEK\luiza.tanasie <luiza.tanasie197@gmail.com>
2017-08-02 11:26:30 +0300

    cetegory icon

--- PayForAdvice/Domain/Category.cs
+++ PayForAdvice/Domain/Category.cs
@@ -7,6 +7,7 @@ namespace Domain
         public string Name { get; set; }
         public string Description { get; set; }
         public string ImageUrl { get; set; }
+        public string IconUrl { get; set; }
 
         //foreign key
         public virtual ICollection<User> Users { get; set; }
